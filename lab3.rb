c7a1a517ca69eefd87a622dbe90e660830368133
alangy <alangy00@gmail.com>
2020-09-19 13:05:32 +0800

    close #4

--- lab3.rb
+++ lab3.rb
@@ -1,29 +1,22 @@
 #puts $x
 #puts "the length is #{$x.length}"
-def check(nowpos)
-  if nowpos==$x.size-1
-    if $x[nowpos]!='0'
-      $ans+=1
-    end
+def check(input)
+  if input.size==0 || input[0]=='0'  #一定不能进行任何匹配
+    $ans=0
     return
   end
-  if nowpos==$x.size-2
-    if $x[nowpos]=='0'
-      return
+  dpans=Array.new(input.size+1,0)
+  dpans[0]=1   #匹配i[0]
+  dpans[1]=1
+  (1..input.size-1).each do |i|   #从i[1]开始判断
+    if input[i].to_i >=1 && input[i].to_i <=9
+      dpans[i+1]+=dpans[i]
     end
-    check(nowpos+1)  #将这个点解析为单个的字母，继续进行迭代
-    if ($x[nowpos..nowpos+1].join.to_i>0)&& ($x[nowpos..nowpos+1].join.to_i<=26)
-      $ans+=1
+    if (input[i-1..i].join.to_i) >0 && (input[i-1..i].join.to_i) <=26 && input[i-1]!='0'
+      dpans[i+1]+=dpans[i-1]
     end
-    return
   end
-  if $x[nowpos]=='0'
-    return
-  end
-  check nowpos+1
-  if ($x[nowpos..nowpos+1].join.to_i>0)&& ($x[nowpos..nowpos+1].join.to_i<=26)
-    check nowpos+2
-  end
+  $ans=dpans[input.size]
 end
 
 $read=gets
@@ -31,7 +24,7 @@ while $read.length!=0
   $ans = 0
   $x = $read.split ""
   $x = $x[0...$x.length - 1]
-  check 0
+  check $x
   puts $ans
   $read = gets
   break if $read.nil?
