0214857ecab4e06ea119f5a4ff27573e9ed05a76
alangy <alangy00@gmail.com>
2020-11-19 02:22:39 +0800

    a

--- blog4/test/controllers/blogs_controller_test.rb
+++ blog4/test/controllers/blogs_controller_test.rb
@@ -4,11 +4,7 @@ class BlogsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @blog = blogs(:one)
   end
-
-  test "should get index" do
-    get blogs_url
-    assert_response :success
-  end
+  
 
 
   test "should show blog" do
